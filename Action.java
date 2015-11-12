afa6a006077a0141217384fb3de20092498c674c
LiliMeng <lilimeng1103@gmail.com>
2015-11-11 21:26:13 -0800

    Create Action.java

---
+++ Action.java
@@ -0,0 +1,14 @@
+package RL;
+
+public class Action
+{
+	public static final int Ahead = 0;
+	public static final int Back = 1;
+	public static final int TurnLeft = 2;
+	public static final int TurnRight = 3;
+	
+	public static final int NumRobotActions = 4;
+	
+	public static final double RobotMoveDistance = 300.0;
+	public static final double RobotTurnDegree = 20.0;
+}
