a21b4c12ff5364c2461a2bb538377cd5d095fa61
LiliMeng <lilimeng1103@gmail.com>
2015-11-12 18:31:47 -0800

    Update Action.java

--- Action.java
+++ Action.java
@@ -1,14 +1,14 @@
-package RL;
+package ReinforcementLearning;
 
-public class Action
+public class RobotAction
 {
 	public static final int Ahead = 0;
 	public static final int Back = 1;
 	public static final int TurnLeft = 2;
 	public static final int TurnRight = 3;
 	
-	public static final int NumRobotActions = 4;
+	public static final int numRobotActions = 4;
 	
-	public static final double RobotMoveDistance = 300.0;
-	public static final double RobotTurnDegree = 20.0;
+	public static final double RobotMoveDistance = 200.0;
+	public static final double RobotTurnDegree = 30.0;
 }
