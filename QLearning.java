fecdb434ae24f97b3ec0be0fe6f61cf355cb28c7
LiliMeng <lilimeng1103@gmail.com>
2015-11-26 10:21:21 -0800

    Update QLearning.java

--- QLearning.java
+++ QLearning.java
@@ -8,10 +8,10 @@ public class QLearning
 {
 	public static final double LearningRate = 0.1;
 	public static final double DiscountRate = 0.9;
-	public double ExploitationRate = 0.8;
+	public double ExploitationRate = 0.01;
 	private int lastState;
 	private int lastAction;
-	private boolean first = true;
+	//private boolean first = true;
 	private LUQTable Qtable;
 
 	public double setExploitationRate(double value)
@@ -26,18 +26,11 @@ public class QLearning
 
 	public void learn(int state, int action, double reward)
 	{
-		System.out.println("reward: " + reward);
-		if(first)
-		{
-			first = false;
-		}
-		else
-		{
-			double oldQValue = Qtable.getQValue(lastState, lastAction);
-			double newQValue = oldQValue + LearningRate * (reward+ DiscountRate * Qtable.maxQValue(state)-oldQValue);
+		double oldQValue = Qtable.getQValue(lastState, lastAction);
+		double newQValue = oldQValue + LearningRate * (reward+ DiscountRate * Qtable.maxQValue(state)-oldQValue);
 			
-			Qtable.setQValue(lastState, lastAction, newQValue);
-		}
+		Qtable.setQValue(lastState, lastAction, newQValue);
+		
 		
 		lastState = state;
 		lastAction = action;
@@ -51,7 +44,7 @@ public class QLearning
 		int actionIndex = 0;
 		
 		if (thres<ExploitationRate)
-		{//exploratory
+		{//randomly select the action(0,1,2,3)
 			Random ran = new Random();
 			actionIndex = ran.nextInt(((RobotAction.numRobotActions-1 - 0) + 1));
 		}
@@ -61,44 +54,5 @@ public class QLearning
 		}
 		return actionIndex;
 	}
-    /*
-	public int selectAction(int state, long time)
-	{
-		double qValue;
-		double sum = 0.0;
-		double[] value = new double[RobotAction.numRobotActions];
-	
-		for(int i =0; i<value.length; i++)
-		{
-			qValue = Qtable.getQValue(state, i);
-			value[i] = Math.exp(ExploitationRate *qValue);
-			sum +=value[i];
-			System.out.println("Q-value: " + qValue);
-		}
-		
-		if(sum!=0)
-		{
-			for(int i = 0; i<value.length; i++)
-			{
-				value[i] /=sum;
-				System.out.println("P(a|s): " + value[i]);
-			}
-		}
-		else
-		{
-			return Qtable.bestAction(state);
-		}
-       
-		int action = 0;
-		double cumProb = 0.0;
-		double randomNum = Math.random();
-		System.out.println("Random Number: " + randomNum);
-		while(randomNum > cumProb && action <value.length)
-		{
-			cumProb +=value[action];
-			action ++;
-		}
-		
-		return action -1;
-	}*/
+   
 }
