4c1c0868bbb119e63d3e52bc546965d3e6a233e0
LiliMeng <lilimeng1103@gmail.com>
2015-11-25 16:38:04 -0800

    Update QLearning.java

--- QLearning.java
+++ QLearning.java
@@ -2,22 +2,23 @@ package ReinforcementLearning;
 
 import ReinforcementLearning.RobotAction;
 import ReinforcementLearning.LUQTable;
+import java.util.Random;
 
 public class QLearning
 {
 	public static final double LearningRate = 0.1;
 	public static final double DiscountRate = 0.9;
-	public static final double ExploitationRate = 1;
+	public static final double ExploitationRate = 0.1;
 	private int lastState;
 	private int lastAction;
 	private boolean first = true;
 	private LUQTable Qtable;
-	
+	public double QValueDiff;
+
 	public QLearning(LUQTable table)
 	{
 		this.Qtable = table;
 	}
-	
 
 	public void learn(int state, int action, double reward)
 	{
@@ -30,19 +31,40 @@ public class QLearning
 		{
 			double oldQValue = Qtable.getQValue(lastState, lastAction);
 			double newQValue = oldQValue + LearningRate * (reward+ DiscountRate * Qtable.maxQValue(state)-oldQValue);
+			
 			Qtable.setQValue(lastState, lastAction, newQValue);
 		}
 		
 		lastState = state;
 		lastAction = action;
 	}
+	
+	
+	public int selectAction(int state){
 
+		double thres = Math.random();
+		
+		int actionIndex = 0;
+		
+		if (thres<ExploitationRate)
+		{//exploratory
+			Random ran = new Random();
+			actionIndex = ran.nextInt(((RobotAction.numRobotActions-1 - 0) + 1));
+		}
+		else
+		{//greedy
+			
+			actionIndex=Qtable.bestAction(state);
+		}
+		return actionIndex;
+	}
+    /*
 	public int selectAction(int state, long time)
 	{
 		double qValue;
 		double sum = 0.0;
 		double[] value = new double[RobotAction.numRobotActions];
-		
+	
 		for(int i =0; i<value.length; i++)
 		{
 			qValue = Qtable.getQValue(state, i);
@@ -63,7 +85,7 @@ public class QLearning
 		{
 			return Qtable.bestAction(state);
 		}
-
+       
 		int action = 0;
 		double cumProb = 0.0;
 		double randomNum = Math.random();
@@ -75,5 +97,5 @@ public class QLearning
 		}
 		
 		return action -1;
-	}
+	}*/
 }
